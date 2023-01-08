22df750c2129a7c85d2b4fb1073c366369f47431
wolf-77 <testnetworkemail@gmail.com>
2023-01-08 15:20:51 +0100

    init-docker

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h3>
+        Hello World
+      </h3>
+      <h6>
+        Docker cheat sheet
+      </h6>
+      <a href='https://kapeli.com/cheat_sheets/Dockerfile.docset/Contents/Resources/Documents/index'>
+        Link
+      </a>
+    </div>
+  );
+}
+
+export default App;
